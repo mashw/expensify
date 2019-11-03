76e6f19a08f1f8fb09459c7d71f12c18119e7acc
randomstuffgenerator <mashwilliams@gmail.com>
2019-11-03 08:21:10 +0000

    Reinitialized due to drive error

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC-evxBOUBNiMBzh6snCFUnCgN8BSL_9SI",
-    authDomain: "expensify-3f3c1.firebaseapp.com",
-    databaseURL: "https://expensify-3f3c1.firebaseio.com",
-    projectId: "expensify-3f3c1",
-    storageBucket: "expensify-3f3c1.appspot.com",
-    messagingSenderId: "828856868663",
-    appId: "1:828856868663:web:83c35c994dc2d429af84b2"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
